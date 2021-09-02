4857e2242e1c02d6ee311c6631f8131cf17923cf
Ciberian <isi04047891@gmail.com>
2021-09-02 06:38:46 +0500

    made club-cards-block

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function myFunction() {
-  var navigation = document.querySelector(".nav-list");
-  var burgerMenu = document.querySelector(".burger-menu");
-  
-  if (navigation.className === "nav-list") {
-    navigation.className += " nav-list--expand";
-  } else {
-    navigation.className = "nav-list";
-  }
-  burgerMenu.classList.toggle('burger-menu--open');
+	var navigation = document.querySelector(".nav-list")
+	var burgerMenu = document.querySelector(".burger-menu")
+
+	if (navigation.className === "nav-list") {
+		navigation.className += " nav-list--expand"
+	} else {
+		navigation.className = "nav-list"
+	}
+	burgerMenu.classList.toggle("burger-menu--open")
 }
