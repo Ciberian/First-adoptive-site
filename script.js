6c32ceb361dc644a23fa92cd1fe8d0ab419705fa
Ciberian <isi04047891@gmail.com>
2021-09-03 05:53:25 +0500

    added imgs for mobile version and made js logic for nav

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
-function myFunction() {
-	var navigation = document.querySelector(".nav-list")
-	var burgerMenu = document.querySelector(".burger-menu")
+var navigation = document.querySelector('.nav-list');
+var burgerMenu = document.querySelector('.burger-menu');
+var navElement = navigation.querySelectorAll('.nav-list__link');
 
-	if (navigation.className === "nav-list") {
-		navigation.className += " nav-list--expand"
+function myFunction() {
+	if (navigation.className === 'nav-list') {
+		navigation.className += ' nav-list--expand';
 	} else {
-		navigation.className = "nav-list"
+		navigation.className = 'nav-list';
 	}
-	burgerMenu.classList.toggle("burger-menu--open")
+	burgerMenu.classList.toggle('burger-menu--open');
+}
+
+
+for (var i=0; i<navElement.length; i++) {
+  navElement[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName('nav-list__link--active');
+    if (current.length > 0) {
+      current[0].className = current[0].className.replace(' nav-list__link--active', '');
+    }
+    this.className += ' nav-list__link--active';
+  });
 }
