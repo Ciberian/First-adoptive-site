81dfda2af92aa0e685d571470b907e34f527b649
Ciberian <isi04047891@gmail.com>
2021-09-05 08:49:44 +0500

    made desktop version for team-block

--- script.js
+++ script.js
@@ -1,76 +1,90 @@
 //-------------------Работа бургер меню------------------------
-var navigation = document.querySelector(".nav-list")
-var burgerMenu = document.querySelector(".burger-menu")
+var navigation = document.querySelector(".nav-list");
+var burgerMenu = document.querySelector(".burger-menu");
 
 function myFunction() {
 	if (navigation.className === "nav-list") {
-		navigation.className += " nav-list--expand"
+		navigation.className += " nav-list--expand";
 	} else {
-		navigation.className = "nav-list"
+		navigation.className = "nav-list";
 	}
-	burgerMenu.classList.toggle("burger-menu--open")
+	burgerMenu.classList.toggle("burger-menu--open");
 }
 
 //----Добавление активного класса выбранному элементу навигации----
-var navElement = navigation.querySelectorAll(".nav-list__link")
+var navElement = navigation.querySelectorAll(".nav-list__link");
+
 for (var i = 0; i < navElement.length; i++) {
 	navElement[i].addEventListener("click", function () {
-		var current = document.getElementsByClassName("nav-list__link--active")
+		var current = document.getElementsByClassName("nav-list__link--active");
 		if (current.length > 0) {
-			current[0].className = current[0].className.replace(" nav-list__link--active", "")
+			current[0].className = current[0].className.replace(" nav-list__link--active", "");
 		}
-		this.className += " nav-list__link--active"
-	})
+		this.className += " nav-list__link--active";
+	});
+}
+
+//----Добавление активного класса выбранному периоду для абонемента----
+var clubCardsPeriod = document.querySelectorAll(".club-cards__period-link");
+
+for (var i = 0; i < clubCardsPeriod.length; i++) {
+	clubCardsPeriod[i].addEventListener("click", function () {
+		var current = document.getElementsByClassName("club-cards__period-link--active");
+		if (current.length > 0) {
+			current[0].className = current[0].className.replace(" club-cards__period-link--active", "");
+		}
+		this.className += " club-cards__period-link--active";
+	});
 }
 
 //-----------Пересчет стоимости абонементов в зависимости от выбранного периода-------
-var oneMonth = document.querySelector(".one-month")
-var halfYear = document.querySelector(".half-year")
-var oneYear = document.querySelector(".one-year")
+var oneMonth = document.querySelector(".one-month");
+var halfYear = document.querySelector(".half-year");
+var oneYear = document.querySelector(".one-year");
 
-var prices = document.querySelectorAll(".club-cards__price")
+var prices = document.querySelectorAll(".club-cards__price");
 
 oneMonth.onclick = function () {
 	for (let i = 0; i < prices.length; i++) {
-		prices[i].textContent = prices[i].dataset.price + " Р"
+		prices[i].textContent = prices[i].dataset.price + " Р";
 	}
-}
+};
 halfYear.onclick = function () {
 	for (let i = 0; i < prices.length; i++) {
-		prices[i].textContent = prices[i].dataset.price * 6 - prices[i].dataset.price * 6 * 0.1 + " Р"
+		prices[i].textContent = prices[i].dataset.price * 6 - prices[i].dataset.price * 6 * 0.1 + " Р";
 	} // Считаем стоимость за 6 месяцев и вычитаем скидку 10%
-}
+};
 oneYear.onclick = function () {
 	for (let i = 0; i < prices.length; i++) {
-		prices[i].textContent = prices[i].dataset.price * 12 - prices[i].dataset.price * 12 * 0.15 + " Р"
+		prices[i].textContent = prices[i].dataset.price * 12 - prices[i].dataset.price * 12 * 0.15 + " Р";
 	} // Считаем стоимость за 12 месяцев и вычитаем скидку 15%
-}
+};
 
 //--------------------------Галерея для блока Наша команда---------------------------------
-var slideIndex = 1
-showSlides(slideIndex)
+var slideIndex = 1;
+showSlides(slideIndex);
 
 // Вперед/назад элементы управления
 function plusSlides(n) {
-	showSlides((slideIndex += n))
+	showSlides((slideIndex += n));
 }
 
 // Элементы управления миниатюрами изображений
 function currentSlide(n) {
-	showSlides((slideIndex = n))
+	showSlides((slideIndex = n));
 }
 
 function showSlides(n) {
-	var i
-	var slides = document.getElementsByClassName("team__slider")
+	var i;
+	var slides = document.querySelectorAll(".team__slide");
 	if (n > slides.length) {
-		slideIndex = 1
+		slideIndex = 1;
 	}
 	if (n < 1) {
-		slideIndex = slides.length
+		slideIndex = slides.length;
 	}
 	for (i = 0; i < slides.length; i++) {
-		slides[i].style.display = "none"
+		slides[i].style.display = "none";
 	}
-	slides[slideIndex - 1].style.display = "block"
+	slides[slideIndex - 1].style.display = "block";
 }
