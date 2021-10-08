40e4f729f01819e06d6a336a009e97da7533c3c4
Ciberian <isi04047891@gmail.com>
2021-10-08 22:57:37 +0500

    changed double brackets to single in js

--- script.js
+++ script.js
@@ -1,70 +1,70 @@
 //---------------------------------------------------------------------------------
 //------------------------------Работа бургер меню---------------------------------
 //---------------------------------------------------------------------------------
-var navigation = document.querySelector(".nav-list");
-var burgerMenu = document.querySelector(".burger-menu");
+var navigation = document.querySelector('.nav-list');
+var burgerMenu = document.querySelector('.burger-menu');
 
 function myFunction() {
-	if (navigation.className === "nav-list") {
-		navigation.className += " nav-list--expand";
+	if (navigation.className === 'nav-list') {
+		navigation.className += ' nav-list--expand';
 	} else {
-		navigation.className = "nav-list";
+		navigation.className = 'nav-list';
 	}
-	burgerMenu.classList.toggle("burger-menu--open");
+	burgerMenu.classList.toggle('burger-menu--open');
 }
 
 //---------------------------------------------------------------------------------
 //------------Добавление активного класса выбранному элементу навигации------------
 //---------------------------------------------------------------------------------
-var navElement = navigation.querySelectorAll(".nav-list__link");
+var navElement = navigation.querySelectorAll('.nav-list__link');
 
 for (let i = 0; i < navElement.length; i++) {
-	navElement[i].addEventListener("click", function () {
-		var current = document.getElementsByClassName("nav-list__link--active");
+	navElement[i].addEventListener('click', function () {
+		var current = document.getElementsByClassName('nav-list__link--active');
 		if (current.length > 0) {
-			current[0].className = current[0].className.replace(" nav-list__link--active", "");
+			current[0].className = current[0].className.replace(' nav-list__link--active', '');
 		}
-		this.className += " nav-list__link--active";
+		this.className += ' nav-list__link--active';
 	});
 }
 
 //---------------------------------------------------------------------------------
 //---------Добавление активного класса выбранному периоду для абонемента-----------
 //---------------------------------------------------------------------------------
-var clubCardsPeriod = document.querySelectorAll(".club-cards__period-link");
+var clubCardsPeriod = document.querySelectorAll('.club-cards__period-link');
 
 for (let i = 0; i < clubCardsPeriod.length; i++) {
-	clubCardsPeriod[i].addEventListener("click", function () {
-		var current = document.getElementsByClassName("club-cards__period-link--active");
+	clubCardsPeriod[i].addEventListener('click', function () {
+		var current = document.getElementsByClassName('club-cards__period-link--active');
 		if (current.length > 0) {
-			current[0].className = current[0].className.replace(" club-cards__period-link--active", "");
+			current[0].className = current[0].className.replace(' club-cards__period-link--active', '');
 		}
-		this.className += " club-cards__period-link--active";
+		this.className += ' club-cards__period-link--active';
 	});
 }
 
 //---------------------------------------------------------------------------------
 //-------Пересчет стоимости абонементов в зависимости от выбранного периода--------
 //---------------------------------------------------------------------------------
-var oneMonth = document.querySelector(".one-month");
-var halfYear = document.querySelector(".half-year");
-var oneYear = document.querySelector(".one-year");
+var oneMonth = document.querySelector('.one-month');
+var halfYear = document.querySelector('.half-year');
+var oneYear = document.querySelector('.one-year');
 
-var prices = document.querySelectorAll(".club-cards__price");
+var prices = document.querySelectorAll('.club-cards__price');
 
 oneMonth.onclick = function () {
 	for (let i = 0; i < prices.length; i++) {
-		prices[i].textContent = prices[i].dataset.price + " Р";
+		prices[i].textContent = prices[i].dataset.price + ' Р';
 	}
 };
 halfYear.onclick = function () {
 	for (let i = 0; i < prices.length; i++) {
-		prices[i].textContent = prices[i].dataset.price * 6 - prices[i].dataset.price * 6 * 0.1 + " Р";
+		prices[i].textContent = prices[i].dataset.price * 6 - prices[i].dataset.price * 6 * 0.1 + ' Р';
 	} // Считаем стоимость за 6 месяцев и вычитаем скидку 10%
 };
 oneYear.onclick = function () {
 	for (let i = 0; i < prices.length; i++) {
-		prices[i].textContent = prices[i].dataset.price * 12 - prices[i].dataset.price * 12 * 0.15 + " Р";
+		prices[i].textContent = prices[i].dataset.price * 12 - prices[i].dataset.price * 12 * 0.15 + ' Р';
 	} // Считаем стоимость за 12 месяцев и вычитаем скидку 15%
 };
 
@@ -85,8 +85,8 @@ function currentSlide(n) {
 }
 
 function showSlides(n) {
-	var slides = document.querySelectorAll(".team__slide");
-	var previews = document.querySelectorAll(".team__preview-item");
+	var slides = document.querySelectorAll('.team__slide');
+	var previews = document.querySelectorAll('.team__preview-item');
 
 	// Механизм перелистывания слайдов
 	if (n > slides.length) {
@@ -98,14 +98,14 @@ function showSlides(n) {
 
 	// Механизм показа слайдов
 	for (let i = 0; i < slides.length; i++) {
-		slides[i].style.display = "none";
+		slides[i].style.display = 'none';
 	}
 
 	for (let i = 0; i < previews.length; i++) {
-		previews[i].className = previews[i].className.replace(" team__preview-item--active", "");
+		previews[i].className = previews[i].className.replace(' team__preview-item--active', '');
 	}
-	slides[slideIndex - 1].style.display = "block";
-	previews[slideIndex - 1].className += " team__preview-item--active";
+	slides[slideIndex - 1].style.display = 'block';
+	previews[slideIndex - 1].className += ' team__preview-item--active';
 }
 
 //---------------------------------------------------------------------------------
@@ -119,7 +119,7 @@ function plusGallerySlides(n) {
 }
 
 function showGallerySlides(n) {
-	var gallerySlides = document.querySelectorAll(".gallery__item");
+	var gallerySlides = document.querySelectorAll('.gallery__item');
 
 	// Механизм перелистывания слайдов
 	if (n > gallerySlides.length) {
@@ -130,22 +130,22 @@ function showGallerySlides(n) {
 	}
 	// Механизм показа слайдов
 	for (let i = 0; i < gallerySlides.length; i++) {
-		gallerySlides[i].style.display = "none";
+		gallerySlides[i].style.display = 'none';
 	}
-	gallerySlides[gallerySlideIndex - 1].style.display = "block";
+	gallerySlides[gallerySlideIndex - 1].style.display = 'block';
 }
 
 // Медиазапрос для слайдера.
 // При ширине экрана 992px и выше запрос возвращает объект MediaQueryList со свойством matches значение которого true.
 // При меньшей ширине экрана значение matches - false
-const mediaQuery = window.matchMedia("(min-width: 992px)");
+const mediaQuery = window.matchMedia('(min-width: 992px)');
 
 function viewportWidthGal(e) {
-	var gallerySlides = document.querySelectorAll(".gallery__item");
+	var gallerySlides = document.querySelectorAll('.gallery__item');
 	if (e.matches) {
 		// Если ширина экрана 992px и больше, то отображаем все элементы галереи
 		for (let i = 0; i < gallerySlides.length; i++) {
-			gallerySlides[i].style.display = "block";
+			gallerySlides[i].style.display = 'block';
 		}
 	} else {
 		// В ином случае вызываем функцию показа слайдов
@@ -154,16 +154,16 @@ function viewportWidthGal(e) {
 }
 
 // Отслеживаем изменение ширины экрана
-mediaQuery.addEventListener("change", viewportWidthGal);
+mediaQuery.addEventListener('change', viewportWidthGal);
 viewportWidthGal(mediaQuery);
 
 //---------------------------------------------------------------------------------
 //--------------------------Слайдер для блока Отзывы-------------------------------
 //---------------------------------------------------------------------------------
-const sliderLine = document.querySelector(".reviews__list");
-const reviews = document.querySelectorAll(".reviews__item");
-const btnNext = document.querySelector(".reviews__button-next");
-const btnPrev = document.querySelector(".reviews__button-prev");
+const sliderLine = document.querySelector('.reviews__list');
+const reviews = document.querySelectorAll('.reviews__item');
+const btnNext = document.querySelector('.reviews__button-next');
+const btnPrev = document.querySelector('.reviews__button-prev');
 
 // Счетчик слайдов
 var count = 0;
@@ -171,7 +171,7 @@ var slideWidth;
 
 // При ширине экрана 992px и выше запрос возвращает объект MediaQueryList со свойством matches значение которого true.
 // При меньшей ширине экрана значение matches - false
-const mediaQuery2 = window.matchMedia("(min-width: 992px)");
+const mediaQuery2 = window.matchMedia('(min-width: 992px)');
 
 function viewportWidthRev(e) {
 	if (e.matches) {
@@ -184,32 +184,32 @@ function viewportWidthRev(e) {
 	rollSlider(); // При ресайзе вьюпорта сбрасываем смещение слайда от предыдущего вьюпорта, вызывая функцию пропрутки. Т.к. при разных вьюпортах разные размер слайда и разный шаг смещения.
 }
 // Отслеживаем изменение ширины экрана и при изменении вызываем функцию viewportWidth
-mediaQuery2.addEventListener("change", viewportWidthRev);
+mediaQuery2.addEventListener('change', viewportWidthRev);
 viewportWidthRev(mediaQuery2);
 
 //Ширина ряда слайдов = ширина отдельного слайда умноженная на их количество
 function init() {
-	sliderLine.style.width = slideWidth * reviews.length + "px";
+	sliderLine.style.width = slideWidth * reviews.length + 'px';
 }
 init();
 
 //Кнопки перелистывания слайдов
 btnNext.onclick = function () {
 	count++;
-	btnPrev.classList.remove("reviews__button-prev--disabled");
+	btnPrev.classList.remove('reviews__button-prev--disabled');
 	if (count >= reviews.length - 1) {
 		count = reviews.length - 1;
-		btnNext.classList.add("reviews__button-next--disabled");
+		btnNext.classList.add('reviews__button-next--disabled');
 	}
 	rollSlider();
 };
 
 btnPrev.onclick = function () {
 	count--;
-	btnNext.classList.remove("reviews__button-next--disabled");
+	btnNext.classList.remove('reviews__button-next--disabled');
 	if (count <= 0) {
 		count = 0;
-		btnPrev.classList.add("reviews__button-prev--disabled");
+		btnPrev.classList.add('reviews__button-prev--disabled');
 	}
 	rollSlider();
 };
@@ -217,5 +217,5 @@ btnPrev.onclick = function () {
 //Задаем смещение на ширину слайда, через трансформацию, хотя можно и через position:relative
 //Ширину слайда задаем через функцию viewportWidth выше, т.к. ширина слайда меняется, в зависимости от ширины вьюпорта
 function rollSlider() {
-	sliderLine.style.transform = "translate(-" + count * slideWidth + "px)";
+	sliderLine.style.transform = 'translate(-' + count * slideWidth + 'px)';
 }
